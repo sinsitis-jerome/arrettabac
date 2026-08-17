7cf67fa871fa58c35160274a4cfd2d85fbcc103a
sinsitis-jerome <sinsitis@gmail.com>
2026-08-17 09:49:53 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'suivi-cig-v1';
+const CACHE_NAME = 'suivi-cig-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
@@ -17,8 +17,16 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
+// Réseau en priorité : on va toujours chercher la dernière version en ligne.
+// Le cache ne sert que de repli si le téléphone est hors-ligne.
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
